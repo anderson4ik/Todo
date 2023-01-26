3cc410f58fb63c2fe867668c7cab6042add05025
Andrei Lapicov <lapicov@gmail.com>
2023-01-26 20:03:47 +0200

    26.03.23 commit

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import SearchPanel from "./components/search-panel";
 const App = () => {
 
     const todoData = [
-        { label: 'Drink Coffee', important: false },
-        { label: 'Drink Tea', important: false },
-        { label: 'Build REACT APP', important: true },
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Drink Tea', important: false, id: 2 },
+        { label: 'Build REACT APP', important: true, id: 3 },
     ]
 
     return (
