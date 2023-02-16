d3478766b043db66cb730c2ae76aac9076626f4e
Andrei Lapicov <lapicov@gmail.com>
2023-02-16 21:21:26 +0200

    160223-2

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return (
-        <input type="text"
-            className="form-control search-input"
-            placeholder="search" />
-    );
-}
-export default SearchPanel;
+export default class SearchPanel extends Component {
+
+    state = {
+        term: '',
+    };
+
+    onChangeSearch = (e) => {
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.onSearchChange(term);
+    }
+
+
+    render() {
+        return (
+            <input type="text"
+                   className="form-control search-input"
+                   placeholder="Search"
+                   value={this.state.term}
+                   onChange={this.onChangeSearch}/>
+        );
+    };
+};
