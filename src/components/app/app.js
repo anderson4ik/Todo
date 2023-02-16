21e9f546fa72daf51166ee113a352514339c716f
Andrei Lapicov <lapicov@gmail.com>
2023-02-16 22:50:46 +0200

    160223-3

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,6 +18,7 @@ export default class App extends Component {
             this.createTodoItem('Build REACT APP'),
         ],
         term: '',
+        filter: 'all',
     };
 
     createTodoItem(label) {
@@ -96,12 +97,31 @@ export default class App extends Component {
            return item.label
                .toLowerCase()
                .indexOf(term.toLowerCase()) > -1;
-       })
-    }
+       });
+    };
+
+    filter(items, filter) {
+        switch (filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        };
+    };
+
+    onFilterChange = (filter) => {
+        this.setState({
+            filter,
+        });
+    };
 
     render() {
-        const { todoData, term } = this.state;
-        const visibleItems = this.search(todoData, term);
+        const { todoData, term, filter } = this.state;
+        const visibleItems = this.filter(this.search(todoData, term), filter);
         const doneCount = todoData.filter((el) => el.done).length;
 
         const todoCount = todoData.length - doneCount;
@@ -112,7 +132,9 @@ export default class App extends Component {
                 <div className="top-panel d-flex" >
                     <SearchPanel
                          onSearchChange={this.onSearchChange}/>
-                    < ItemStatusFilter />
+                    < ItemStatusFilter
+                         filter={ filter }
+                         onFilterChange={this.onFilterChange}/>
                 </div>
 
                 <TodoList
